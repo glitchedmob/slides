f0b33c6a8eca131809305be4a776a5ed4fee48e1
Levi Zitting <me@levizitting.com>
2023-03-01 10:51:02 -0600

    feat: styling for slide landing page

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -5,5 +5,37 @@ useTitle('Levi Zitting Slides');
 </script>
 
 <template>
-    <RouterLink :to="{ name: 'DotNetAndWhyYouShouldCare' }">.NET And Why You Should Care</RouterLink>
+    <div class="home">
+        <div class="container">
+            <div class="container">
+                <h1>Slides</h1>
+                <RouterLink class="link" :to="{ name: 'DotNetAndWhyYouShouldCare' }">
+                    .NET And Why You Should Care
+                </RouterLink>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped lang="scss">
+.home {
+    background-color: var(--dark-blue);
+    width: 100%;
+    height: 100%;
+
+    h1 {
+        color: var(--white);
+        font-family: var(--heading-font);
+        font-weight: var(--heading-font-weight);
+        font-size: 50px;
+        text-align: center;
+        padding: 50px 0;
+    }
+}
+
+.link {
+    color: var(--white);
+    font-family: var(--main-font);
+    font-size: 25px;
+}
+</style>
--- src/composables/useReveal.ts
+++ src/composables/useReveal.ts
@@ -1,6 +1,7 @@
 import { ref } from 'vue';
 import Reveal from 'reveal.js';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
+import RevealNotes from 'reveal.js/plugin/notes/notes';
 
 type RevealOnReadyCallback = (reveal: Reveal.Api) => void | Promise<void>;
 
@@ -28,7 +29,7 @@ export const useReveal = () => {
             center: true,
             embedded: true,
             controlsTutorial: false,
-            plugins: [RevealHighlight],
+            plugins: [RevealHighlight, RevealNotes],
         });
 
         reveal.value = revealInstance;
