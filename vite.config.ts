8fbe22bac1ba404360e87f3901c78fa93cd65d47
Levi Zitting <me@levizitting.com>
2025-02-26 00:26:06 -0600

    chore: upgrade packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         },
     },
-});
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id: string) {
+                    if (id.includes('reveal.js')) {
+                        return 'reveal';
+                    }
+                }
+            }
+        }
+    }
+})
